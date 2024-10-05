b9d88405bc9ad485906c395757375be39d1f678f
DMoscicki <dimulgator@outlook.com>
2024-10-06 00:59:33 +0300

    feat!: add namespace mod

---
+++ src/handlers/namespaces.rs
@@ -0,0 +1,26 @@
+use actix_web::{get, http::StatusCode, web, HttpResponse, Responder, Result};
+use kube::Client;
+use ns_api::{create_ns, get_all_namespaces};
+
+mod ns_api;
+
+#[get("/namespaces")]
+async fn get_all_ns(kube_state: web::Data<Client>) -> Result<impl Responder> {
+    let client = kube_state.get_ref();
+
+    let namespaces = get_all_namespaces(client).await;
+
+    Ok(web::Json(namespaces))
+}
+
+#[get("/namespaces/{name}")]
+async fn create_namespace(kube_state: web::Data<Client>, param: web::Path<String>) -> HttpResponse {
+    let client = kube_state.get_ref();
+    let name = param.into_inner();
+
+    match create_ns(client, name).await {
+        true => HttpResponse::Ok().status(StatusCode::OK).finish(),
+        false => HttpResponse::BadRequest().status(StatusCode::BAD_REQUEST).finish(),
+    }
+
+}
---
+++ src/handlers/namespaces/ns_api.rs
@@ -0,0 +1,27 @@
+use k8s_openapi::api::core::v1::Namespace;
+use kube::{api::{ObjectList, PostParams}, Api, Client};
+
+
+pub async fn get_all_namespaces(client: &Client) -> ObjectList<Namespace> {
+    let namespace_api: Api<Namespace> = Api::all(client.clone());
+
+    let ns_list = namespace_api.list(&Default::default()).await.unwrap();
+
+    ns_list
+}
+
+pub async fn create_ns(client: &Client, ns: String) -> bool {
+    let res: Api<Namespace> = Api::all(client.clone());
+
+    let pp = PostParams::default();
+
+    let mut new_ns = Namespace::default();
+
+    new_ns.metadata.name = Some(ns);
+
+    let result = res.create(&pp, &new_ns).await;
+    match result {
+        Ok(_) => return true,
+        Err(_) => return false,
+    }
+}
