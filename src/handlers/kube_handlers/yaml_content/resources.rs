8fddf015f844d6808719f025bef5a7494a047bfd
DMoscicki <dimulgator@outlook.com>
2025-02-09 12:14:24 +0300

    feat: :sparkles: add dynamic yaml apply

--- src/handlers/kube_handlers/yaml_content/resources.rs
+++ src/handlers/kube_handlers/yaml_content/resources.rs
@@ -4,6 +4,9 @@ use kube::{
     api::{ApiResource, DynamicObject, GroupVersionKind, Patch, PatchParams},
     Api, Client, Discovery,
 };
+use kube::api::TypeMeta;
+use kube::discovery::{ApiCapabilities, Scope};
+use kube::error::DiscoveryError;
 use log::info;
 
 pub struct KubeObjectType {
@@ -19,22 +22,19 @@ impl KubeObjectType {
     }
 
     pub async fn apply(&self) -> Result<(), kube::Error> {
-        let mut plural = String::from(&self.content.types.clone().unwrap().kind.to_lowercase());
-        plural.push('s');
+        let dvry = Discovery::new(self.client.clone()).run().await?;
 
+        let (ar,caps) = check_gvk(self.content.types.clone(), dvry)?;
 
-        // TODO: Add Cluster Scope and namespace checking
-        let api_cl: Api<DynamicObject> = Api::namespaced_with(
-            self.client.clone(),
-            &self.content.metadata.namespace.clone().unwrap(),
-            &ApiResource {
-                group: "".to_string(),
-                api_version: self.content.types.clone().unwrap().api_version,
-                kind: self.content.types.clone().unwrap().kind,
-                version: self.content.types.clone().unwrap().api_version,
-                plural,
-            },
-        );
+        let api_cl: Api<DynamicObject> = if let Some(namespace) = &self.content.metadata.namespace {
+            if caps.scope == Scope::Cluster {
+                Api::all_with(self.client.clone(), &ar)
+            } else {
+                Api::namespaced_with(self.client.clone(), namespace, &ar)
+            }
+        } else {
+            Api::default_namespaced_with(self.client.clone(), &ar)
+        };
 
         let res = api_cl
             .patch(
@@ -43,8 +43,6 @@ impl KubeObjectType {
                 &Patch::Apply(&self.content),
             )
             .await;
-
-        info!("{:#?}", res);
 
         match res {
             Ok(_) => Ok(()),
@@ -56,4 +54,15 @@ impl KubeObjectType {
     //     let cl: Api<DynamicObject> = Api::namespaced_with(client, namespace, &self.content_type);
     //     Ok(())
     // }
+}
+
+fn check_gvk(tp: Option<TypeMeta>, discovery: Discovery) -> Result<(ApiResource, ApiCapabilities), kube::Error>{
+    let gvk = if let Some(tm) = tp {
+        GroupVersionKind::try_from(tm).unwrap()
+    } else {
+        let e = kube::Error::Discovery(DiscoveryError::InvalidGroupVersion("error to get gvk".to_string()));
+        return Err(e)
+    };
+
+    Ok(discovery.resolve_gvk(&gvk).unwrap())
 }
