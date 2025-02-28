066a6192f66bb2123bae5e730df0cafe7ed299f0
DMoscicki <dimulgator@outlook.com>
2025-02-28 19:05:23 +0300

    feat: change log to tracing

--- src/handlers/kube_handlers/configmaps/kube_configmaps.rs
+++ src/handlers/kube_handlers/configmaps/kube_configmaps.rs
@@ -3,7 +3,7 @@ use std::io;
 use k8s_openapi::api::core::v1::ConfigMap;
 use k8s_rs_pb::api::core::v1::ConfigMapList;
 use kube::{Api, Client};
-use log::error;
+use tracing::error;
 
 
 pub async fn get_all_configmaps(client: &Client) -> Result<ConfigMapList, io::Error> {
